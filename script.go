169d9db1bb3af68710e7244ce479b7d49a7abf37
berquerant <sin@sin-pro.local>
2024-04-14 17:21:44 +0900

    Add KeepScriptFile option to Script

--- script.go
+++ script.go
@@ -1,7 +1,7 @@
 package execx
 
 import (
-	"io"
+	"fmt"
 	"os"
 )
 
@@ -12,6 +12,11 @@ type Script struct {
 	// Content is a set of commands.
 	Content string
 	Env     Env
+	// If KeepScriptFile is true, then do not regenerate script files,
+	// and not reflect changes in Content and Env when calling Runner.
+	KeepScriptFile bool
+
+	script *scriptFile
 }
 
 // NewScript creates a new [Script].
@@ -23,25 +28,94 @@ func NewScript(content string, shell string, arg ...string) *Script {
 	}
 }
 
-// Runner creates a new [Cmd] and pass it to f.
-func (s Script) Runner(f func(*Cmd) error) error {
-	fp, err := os.CreateTemp("", "execx")
+// Close removes a temporary script file.
+func (s *Script) Close() error {
+	if s.script != nil {
+		x := s.script
+		s.script = nil
+		return x.close()
+	}
+	return nil
+}
+
+func (s Script) isExecutable() bool {
+	return s.script != nil && s.script.isExecutable()
+}
+
+func (s Script) requireNewScript() bool {
+	return !s.isExecutable() || !s.KeepScriptFile
+}
+
+func (s *Script) newScript() error {
+	if !s.requireNewScript() {
+		return nil
+	}
+
+	content := s.Env.Expand(s.Content)
+	f, err := newScriptFile(content)
 	if err != nil {
 		return err
 	}
-	defer func() {
-		_ = os.Remove(fp.Name())
-	}()
+	s.script = f
+	return nil
+}
 
-	cmd := New(s.Shell[0], append(s.Shell[1:], fp.Name())...)
+func (s *Script) prepare() (*Cmd, error) {
+	if err := s.newScript(); err != nil {
+		return nil, err
+	}
+	cmd := New(s.Shell[0], append(s.Shell[1:], s.script.path)...)
 	cmd.Env.Merge(s.Env)
+	return cmd, nil
+}
 
-	if _, err := io.WriteString(fp, cmd.Env.Expand(s.Content)); err != nil {
-		return err
-	}
-	if err := os.Chmod(fp.Name(), 0755); err != nil {
-		return err
+// Runner creates a new [Cmd] and pass it to f.
+func (s *Script) Runner(f func(*Cmd) error) error {
+	cmd, err := s.prepare()
+	if err != nil {
+		return fmt.Errorf("%w: prepare script file", err)
 	}
 
+	if !s.KeepScriptFile {
+		defer s.Close()
+	}
 	return f(cmd)
+}
+
+type scriptFile struct {
+	path string
+}
+
+func newScriptFile(content string) (*scriptFile, error) {
+	f, err := os.CreateTemp("", "execx")
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	if _, err := f.WriteString(content); err != nil {
+		return nil, err
+	}
+	if err := os.Chmod(f.Name(), 0755); err != nil {
+		return nil, err
+	}
+	return &scriptFile{
+		path: f.Name(),
+	}, nil
+}
+
+func (s scriptFile) close() error {
+	return os.Remove(s.path)
+}
+
+func (s scriptFile) isExecutable() bool {
+	return isExecutable(s.path)
+}
+
+func isExecutable(path string) bool {
+	f, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return !f.IsDir() && f.Mode() == 0755
 }
--- script_test.go
+++ script_test.go
@@ -13,46 +13,106 @@ import (
 )
 
 func TestScript(t *testing.T) {
-	t.Run("Run", func(t *testing.T) {
-		os.Setenv("test_script_append_env1", "append1")
-		const (
-			content = `echo line1
+	assertScriptStdout := func(t *testing.T, want io.Reader, script *execx.Script) {
+		var stdout io.Reader
+		assert.Nil(t, script.Runner(func(cmd *execx.Cmd) error {
+			f, err := cmd.Run(context.TODO())
+			if err != nil {
+				return err
+			}
+			stdout = f.Stdout
+			return nil
+		}))
+		assertReader(t, want, stdout)
+	}
+
+	t.Run("Runner", func(t *testing.T) {
+		t.Run("ChangeScript", func(t *testing.T) {
+			t.Run("KeepScript", func(t *testing.T) {
+				script := execx.NewScript("echo keep", "sh")
+				script.KeepScriptFile = true
+
+				assertScriptStdout(t, bytes.NewBufferString("keep\n"), script)
+				script.Content = "echo changed"
+				assertScriptStdout(t, bytes.NewBufferString("keep\n"), script)
+			})
+
+			script := execx.NewScript("", "sh")
+
+			for _, tc := range []struct {
+				name    string
+				content string
+				env     execx.Env
+				want    io.Reader
+			}{
+				{
+					name:    "echo 1st",
+					content: "echo $v",
+					env:     execx.EnvFromSlice([]string{"v=1"}),
+					want:    bytes.NewBufferString("1\n"),
+				},
+				{
+					name:    "change env",
+					content: "echo $v",
+					env:     execx.EnvFromSlice([]string{"v=2"}),
+					want:    bytes.NewBufferString("2\n"),
+				},
+				{
+					name:    "change script",
+					content: "echo ${v}!",
+					env:     execx.EnvFromSlice([]string{"v=2"}),
+					want:    bytes.NewBufferString("2!\n"),
+				},
+			} {
+				t.Run(tc.name, func(t *testing.T) {
+					script.Content = tc.content
+					script.Env.Merge(tc.env)
+					assertScriptStdout(t, tc.want, script)
+				})
+			}
+		})
+
+		t.Run("Run", func(t *testing.T) {
+			os.Setenv("test_script_append_env1", "append1")
+			const (
+				content = `echo line1
 echo ${line2}
 echo ${test_script_append_env1}
 cat -
 echo line3 > /dev/stderr`
-			stdin      = "from stdin\n"
-			wantStdout = `line1
+				stdin      = "from stdin\n"
+				wantStdout = `line1
 LINE2
 added:append1
 from stdin
 `
-			wantStderr = `line3
+				wantStderr = `line3
 `
-		)
-		script := execx.NewScript(content, "sh")
-		script.Env.Merge(execx.EnvFromEnviron())
-		script.Env.Set("line2", "LINE2")
-		script.Env.Set("test_script_append_env1", "added:${test_script_append_env1}")
+			)
+			script := execx.NewScript(content, "sh")
+			script.Env.Merge(execx.EnvFromEnviron())
+			script.Env.Set("line2", "LINE2")
+			script.Env.Set("test_script_append_env1", "added:${test_script_append_env1}")
 
-		var (
-			stdout io.Reader
-			stderr io.Reader
-		)
+			var (
+				stdout io.Reader
+				stderr io.Reader
+			)
 
-		assert.Nil(t, script.Runner(func(cmd *execx.Cmd) error {
-			cmd.Stdin = bytes.NewBufferString(stdin)
-			r, err := cmd.Run(context.TODO())
-			if err != nil {
-				return err
-			}
-			stdout = r.Stdout
-			stderr = r.Stderr
-			return nil
-		}))
+			assert.Nil(t, script.Runner(func(cmd *execx.Cmd) error {
+				cmd.Stdin = bytes.NewBufferString(stdin)
+				r, err := cmd.Run(context.TODO())
+				if err != nil {
+					return err
+				}
+				stdout = r.Stdout
+				stderr = r.Stderr
+				return nil
+			}))
 
-		assertReader(t, bytes.NewBufferString(wantStdout), stdout)
-		assertReader(t, bytes.NewBufferString(wantStderr), stderr)
+			assertReader(t, bytes.NewBufferString(wantStdout), stdout)
+			assertReader(t, bytes.NewBufferString(wantStderr), stderr)
+		})
 	})
 }
 
